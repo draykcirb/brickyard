8349f1a445f3bcf782616ed22b91d7e78248c543
e-cloud <saintscott119@gmail.com>
2016-07-05 21:40:07 +0800

    feat: decouple commands
    * migrate command in lib to separate repos
    * adjust npm config (package.json)
    * adjust cli.js to detect the commands correctly
    * adjust cli.js specification

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -12,22 +12,12 @@ const Command = require('commander').Command
 describe('#CLI test', function () {
 	let currentDirData = {}
 
-	before('collect current dir data', function () {
-		const cliModulePath = require.resolve('../lib/cli')
-		currentDirData.paths = fs.readdirSync(path.join(path.dirname(cliModulePath), 'commands'))
-	})
-
-	after('', function () {
-
-	})
-
 	it('should load all the commands in the commandStore', function () {
 		const command = new Command('')
 
 		cli.load(command, {}, function () {})
 
-		expect(Object.keys(cli.commands)).to.have.lengthOf(currentDirData.paths.length)
-
+		expect(Object.keys(cli.commands)).to.have.lengthOf(1)
 	})
 
 })
