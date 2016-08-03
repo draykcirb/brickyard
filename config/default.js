195dd4bdf21b8e6f4fdf17921547a9b6dfe0ac5a
e-cloud <saintscott119@gmail.com>
2016-08-03 18:21:48 +0800

    feat: add framework default config and remove old default config logic

---
+++ config/default.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+	programStore: './recipes',
+	pluginStore: './bricks',
+	destPrefix: 'build',
+
+	// path related
+	dest: null,
+	program: null,
+
+	// server related
+	port: 8080,
+	host: 'localhost',
+	bsProxy: {
+		port: 3000,
+		host: 'localhost'
+	},
+	apiProxy: null,
+	watch: false,
+	livereload: false,
+	https: false,
+
+	// others
+	hashbit: 7,
+	lint: false,
+	offline: false,
+	registry: null,
+	showConfig: false,
+	debuggable: false,
+	compress: true
+}
