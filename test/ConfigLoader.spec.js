044498100d14d5e8193d210ebef176ef519786fd
e-cloud <saintscott119@gmail.com>
2017-03-14 16:36:16 +0800

    test: adjust tests for previous changes

--- test/ConfigLoader.spec.js
+++ test/ConfigLoader.spec.js
@@ -8,31 +8,6 @@ const expect = require('chai').expect
 const loader = require('../lib/configLoader')
 
 describe('#Config file loader test', function () {
-
-    describe('test with full config file stack', function () {
-        beforeEach('fake the cwd', function () {
-            process.chdir('./test-resources/fake-brickyard')
-        })
-
-        afterEach('restore the cwd', function () {
-            process.chdir('../../')
-        })
-
-        it('should load the config file on `cwd`', function () {
-
-            const config = loader.run('./config.js')
-            expect(config.port).to.be.equal(3002)
-
-        })
-
-        it('should load the specified config file like `./config/production/config.js`', function () {
-
-            const config = loader.run('./config/production/config.js')
-            expect(config.msg).to.be.equal(123456)
-
-        })
-    })
-
     describe('test with only default config file', function () {
         beforeEach('', function () {
             process.chdir('./test-resources/fake-brickyard2')
@@ -44,7 +19,7 @@ describe('#Config file loader test', function () {
 
         it('should load the default config file', function () {
 
-            const config = loader.run()
+            const config = loader.loadDefaultConfig()
             expect(config.destPrefix).to.equal('build')
 
         })
--- test/cli.spec.js
+++ test/cli.spec.js
@@ -10,19 +10,18 @@ const fs = require('fs')
 const Command = require('commander').Command
 
 describe('#CLI test', function () {
+    beforeEach('', function () {
+        process.chdir('./test-resources/fake-brickyard')
+    })
+
+    afterEach('', function () {
+        process.chdir('../../')
+    })
 
     it('should load all the commands in the commandStore', function () {
         const command = new Command('test')
 
         cli.load(command, null)
-
-        expect(Object.keys(cli.commands)).to.eql(['init'])
-    })
-
-    it('should load all the specified commands', function () {
-        const command = new Command('test')
-
-        cli.load(command, ['brickyard-command-init'])
 
         expect(Object.keys(cli.commands)).to.eql(['init'])
     })
